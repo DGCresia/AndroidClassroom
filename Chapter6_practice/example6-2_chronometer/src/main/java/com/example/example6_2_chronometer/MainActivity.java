148f536d4e6f4e077846ca4dad21600370bb45a3
gami0512 <DragonGodGM@users.noreply.github.com>
2020-04-16 12:52:46 +0900

    chapter6-2_Chronometer

--- Chapter6_practice/example6-2_chronometer/src/main/java/com/example/example6_2_chronometer/MainActivity.java
+++ Chapter6_practice/example6-2_chronometer/src/main/java/com/example/example6_2_chronometer/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.example6_2_chronometer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3;
+    Chronometer cm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        cm = findViewById(R.id.cm);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn1:
+                        cm.start();
+                        break;
+                    case R.id.btn2:
+                        cm.stop();
+                        break;
+                    case R.id.btn3:
+                        cm.setBase(SystemClock.elapsedRealtime());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+
+        btn1.setOnClickListener(listener);
+        btn2.setOnClickListener(listener);
+        btn3.setOnClickListener(listener);
     }
 }
