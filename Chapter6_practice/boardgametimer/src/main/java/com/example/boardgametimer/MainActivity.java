d3a405b75f498a7397438482a9fed0dbf54b1c6b
gami0512 <DragonGodGM@users.noreply.github.com>
2020-04-23 12:28:54 +0900

    Edit BoardgameTimer

--- Chapter6_practice/boardgametimer/src/main/java/com/example/boardgametimer/MainActivity.java
+++ Chapter6_practice/boardgametimer/src/main/java/com/example/boardgametimer/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.boardgametimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     Button timer, reset, pauseRestart;
     TextView corpyright;
     CountDownTimer countDownTimer;
+    TextToSpeech textToSpeech;
+
     long fullTime, halfTime;
     boolean isPaused;
     //endregion
@@ -36,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
         reset = findViewById(R.id.btnReset);
         pauseRestart = findViewById(R.id.btnPauseRestart);
         corpyright = findViewById(R.id.textViewCopyright);
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
         //endregion
 
         //region
@@ -57,11 +72,25 @@ public class MainActivity extends AppCompatActivity {
         reset.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                resetTimer();
-                cancelTimer();
+                if (editText.getText().equals("")) {
+                    Toast.makeText(MainActivity.this, "입력값이 없습니다.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (editText.hasFocus()) {
+                        editText.clearFocus();
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                    }
 
-                timer.setEnabled(true);
-                return false;
+                    resetTimer();
+                    cancelTimer();
+
+                    String msg = String.format("Reset to %d seconds", fullTime);
+                    textToSpeech.speak(msg, TextToSpeech.QUEUE_FLUSH, null);
+
+                    timer.setEnabled(true);
+                }
+                return true;
             }
         });
         //endregion
@@ -90,6 +119,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTick(long millisUntilFinished) {
                 long currentTime = millisUntilFinished/1000;
+                changeTextSize(currentTime);
+
+                if (currentTime == halfTime) {
+                    MediaPlayer.create(getApplicationContext(), R.raw.warning_sound).start();
+                }
+                if (currentTime <= 10) {
+                    timer.setTextColor(Color.RED);
+                    textToSpeech.speak(String.valueOf(currentTime), TextToSpeech.QUEUE_FLUSH, null);
+                }
+
                 timer.setText(String.valueOf(currentTime));
             }
 
@@ -108,11 +147,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void resetTimer() {
+
         fullTime = Long.parseLong(editText.getText().toString());
         halfTime = Math.round(fullTime/2);
+        changeTextSize(fullTime);
 
         timer.setBackgroundColor(Color.YELLOW);
         timer.setTextColor(Color.BLACK);
         timer.setText(String.valueOf(fullTime));
+    }
+    private void changeTextSize(long seconds) {
+        if (seconds>=100) {
+            timer.setTextSize(200);
+        }
+        else if(seconds>=10) {
+            timer.setTextSize(300);
+        }
+        else timer.setTextSize(400);
     }
 }
